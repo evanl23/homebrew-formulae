9c95c1cdd2bd55db4ddb865dc0b1927f82bf70b1
Evan Liu <159852234+evanl23@users.noreply.github.com>
2025-07-09 17:31:01 -0700

    Update terminal-dash.rb

--- terminal-dash.rb
+++ terminal-dash.rb
@@ -1,8 +1,8 @@
 class GeometryDashTerminal < Formula
   desc "An ASCII version of Geometry Dash by RobTop built for your terminal"
   homepage "https://github.com/evanl23/geometry-dash-terminal"
-  url "https://github.com/evanl23/geometry-dash-terminal/archive/refs/tags/v1.0.tar.gz"
-  sha256 "b329f2828e5f1114264c809b0363d58da14a0c9d00c926452005654c878759d1"
+  url "https://github.com/evanl23/geometry-dash-terminal/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "9ebbe2941a6e799cbf8e6b7bfe8817e1d3e91fcc6cd630d079890f07de3d788e"
   license "MIT"
 
   depends_on "ncurses"
