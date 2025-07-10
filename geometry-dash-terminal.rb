cbc5c8a1ed77ce6d2010b8ecc84afd881f3e46f7
Evan Liu <159852234+evanl23@users.noreply.github.com>
2025-07-09 17:24:50 -0700

    Update geometry-dash-terminal.rb

--- geometry-dash-terminal.rb
+++ geometry-dash-terminal.rb
@@ -1,7 +1,7 @@
 class GeometryDashTerminal < Formula
   desc "An ASCII version of Geometry Dash by RobTop built for your terminal"
-  homepage "https://github.com/evanl23/GeometryDash"
-  url "https://github.com/evanl23/GeometryDash/archive/refs/tags/v1.0.tar.gz"
+  homepage "https://github.com/evanl23/geometry-dash-terminal"
+  url "https://github.com/evanl23/geometry-dash-terminal/archive/refs/tags/v1.0.tar.gz"
   sha256 "b329f2828e5f1114264c809b0363d58da14a0c9d00c926452005654c878759d1"
   license "MIT"
 
@@ -9,14 +9,14 @@ class GeometryDashTerminal < Formula
 
   def install
     system "make"
-    bin.install "dash"
+    bin.install "terminal-dash"
     pkgshare.install "levels/stereomadness.txt"
   end
 
   def caveats
     <<~EOS
       To play the game:
-        dash
+        terminal-dash
 
       The level file is installed at:
         #{opt_pkgshare}/stereomadness.txt
