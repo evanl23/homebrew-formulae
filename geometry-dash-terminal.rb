3b10330491790d603df7fc9ce79d4b1c0d4261bf
evanliu23 <eliu23@bu.edu>
2025-07-09 17:03:35 -0700

    Add geometry-dash-terminal formula

---
+++ geometry-dash-terminal.rb
@@ -0,0 +1,25 @@
+class GeometryDashTerminal < Formula
+  desc "An ASCII version of Geometry Dash by RobTop built for your terminal"
+  homepage "https://github.com/evanl23/GeometryDash"
+  url "https://github.com/evanl23/GeometryDash/archive/refs/tags/v1.0.tar.gz"
+  sha256 "b329f2828e5f1114264c809b0363d58da14a0c9d00c926452005654c878759d1"
+  license "MIT"
+
+  depends_on "ncurses"
+
+  def install
+    system "make"
+    bin.install "GeometryDash"
+    pkgshare.install "stereomadness.txt"
+  end
+
+  def caveats
+    <<~EOS
+      To play the game:
+        GeometryDash
+
+      The level file is installed at:
+        #{opt_pkgshare}/stereomadness.txt
+    EOS
+  end
+end
